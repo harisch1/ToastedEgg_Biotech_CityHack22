8057133870bae342958d4e268884fbdca7ad8d0c
harisch1 <haris.choudhary@outlook.com>
2022-01-29 22:13:35 +0800

    firebase config

---
+++ studybuddy/src/components/firebase/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmTUffHmqEDFhfe7ta9qJQglJ5GTP5Cv4",
+  authDomain: "studybuddy-23b0f.firebaseapp.com",
+  projectId: "studybuddy-23b0f",
+  storageBucket: "studybuddy-23b0f.appspot.com",
+  messagingSenderId: "349356323252",
+  appId: "1:349356323252:web:b1a1704fb4f3160281c2e1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);

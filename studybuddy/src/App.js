fb12f71d420accc273acc17973fb5019472fdf56
harisch1 <haris.choudhary@outlook.com>
2022-01-30 03:05:57 +0800

    Sign Up and Sign in implemented

--- studybuddy/src/App.js
+++ studybuddy/src/App.js
@@ -1,34 +1,46 @@
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Chat from "./components/Chat";
 import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AuthProvider } from "./components/AuthContext";
-
+import SignUp from "./components/SignUp";
+import { onAuthStateChanged } from "firebase/auth"; //To see if user is logged in or not
+import { auth } from "./components/firebase/firebase-config";
 const App = () => {
+  //see if user logged in
+  const [user, setUser] = useState({});
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
   return (
-    <AuthProvider>
-      <div className="App">
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Study Buddy</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Chat</Nav.Link>
-              <Nav.Link href="#pricing">Feature x</Nav.Link>
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Study Buddy</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Chat</Nav.Link>
+            <Nav.Link href="#pricing">Feature x</Nav.Link>
+          </Nav>
+          <span style={{ color: "white" }}>Logged in as: {user?.email}</span>
+          {user ? (
+            <Nav>
+              <Nav.Link href="#deets">Sign Out</Nav.Link>
             </Nav>
+          ) : (
             <Nav>
               <Nav.Link href="#deets">Sign In</Nav.Link>
             </Nav>
-          </Container>
-        </Navbar>
-        <Home />
-        {/* <SignUp /> */}
-      </div>
-    </AuthProvider>
+          )}
+        </Container>
+      </Navbar>
+      <Home />
+      <SignIn />
+    </div>
   );
 };
 
--- studybuddy/src/components/SignUp.js
+++ studybuddy/src/components/SignUp.js
@@ -1,76 +1,138 @@
-import React, { useRef, useState } from "react";
-import { useAuth } from "./AuthContext";
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+// import "./App.css";
+import { auth } from "./firebase/firebase-config";
 import { Alert, Card, Form, Button } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
 
-const SignUp = () => {
-  const name = useRef();
-  const University = useRef();
-  const Country = useRef();
-  const email = useRef();
-  const password = useRef();
-  const confPassword = useRef();
-  const { signup } = useAuth();
-  const [err, setErr] = useState("");
+const Signu = () => {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
 
-  async function submit(e) {
-    e.preventDefault();
-    if (password.current.value !== confPassword.current.value) {
-      return;
-    }
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  const register = async () => {
     try {
-      setErr("");
       setLoading(true);
-      await signup(email.current.value, password.current.value);
-    } catch {
-      setErr("Failed to Create Account");
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(user);
+    } catch (error) {
+      setError("Unable to Sign Up");
     }
     setLoading(false);
-  }
+  };
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {err && <Alert variant="danger">{err}</Alert>}
-          <Form.Group id="Name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" ref={name} required />
-          </Form.Group>
-          <Form.Group id="University">
-            <Form.Label>University</Form.Label>
-            <Form.Control type="text" ref={University} required />
-          </Form.Group>
-          <Form.Group id="Country">
-            <Form.Label>Country</Form.Label>
-            <Form.Control type="text" ref={Country} required />
-          </Form.Group>
-          <Form onSubmit={submit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={email} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={password} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={confPassword} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
+    <div className="App">
+      <div>
+        <h3> Register User </h3>
+        <input
+          placeholder="Email..."
+          onChange={(event) => {
+            setRegisterEmail(event.target.value);
+          }}
+        />
+        <input
+          placeholder="Password..."
+          onChange={(event) => {
+            setRegisterPassword(event.target.value);
+          }}
+        />
+
+        <button onClick={register}> Create User</button>
       </div>
-    </>
+      <div>
+        <h3> Login </h3>
+        <input
+          placeholder="Email..."
+          onChange={(event) => {
+            setLoginEmail(event.target.value);
+          }}
+        />
+        <input
+          placeholder="Password..."
+          onChange={(event) => {
+            setLoginPassword(event.target.value);
+          }}
+        />
+
+        <button onClick={login}> Login</button>
+      </div>
+      <h4> User Logged In: </h4>
+      {user?.email}
+      <button onClick={logout}> Sign Out </button>
+      <>
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-4">Sign Up</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+
+            <Form onSubmit={register}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  onChange={(event) => {
+                    setRegisterEmail(event.target.value);
+                  }}
+                  required
+                />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  onChange={(event) => {
+                    setRegisterPassword(event.target.value);
+                  }}
+                  required
+                />
+              </Form.Group>
+              <Button disabled={loading} className="w-100" type="submit">
+                Sign Up
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          Already have an account? <a href="#signin">Sign In</a>
+        </div>
+      </>
+      ;
+    </div>
   );
 };
 
-export default SignUp;
+export default Signu;

86a3291e230dd0c9778ecb7eff4a6733f7781a60
harisch1 <haris.choudhary@outlook.com>
2022-01-30 01:07:01 +0800

    Bug fix env variable

--- studybuddy/src/App.js
+++ studybuddy/src/App.js
@@ -6,26 +6,29 @@ import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "./components/AuthContext";
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Study Buddy</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Chat</Nav.Link>
-            <Nav.Link href="#pricing">Feature x</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">Sign In</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <SignUp />
-      {/* <Home /> */}
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Study Buddy</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Chat</Nav.Link>
+              <Nav.Link href="#pricing">Feature x</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Sign In</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Home />
+        {/* <SignUp /> */}
+      </div>
+    </AuthProvider>
   );
 };
 

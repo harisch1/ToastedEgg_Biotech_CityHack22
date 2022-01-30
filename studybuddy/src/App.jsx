46060c6eb509865efd60f8de58d7afd096e8e3b8
masterTofu <atlanticaxolotl@gmail.com>
2022-01-30 13:19:19 +0800

    video link added

--- studybuddy/src/App.jsx
+++ studybuddy/src/App.jsx
@@ -3,11 +3,9 @@ import { Header } from './components/header'
 import SignUp from './components/SignUp'
 import { Team } from './components/Team'
 import { About } from './components/about'
-import JsonData from './data/data.json'
 import SmoothScroll from 'smooth-scroll'
 import { useState, useEffect } from "react";
 import JsonData from "./data/data.json";
-import SmoothScroll from "smooth-scroll";
 import Chat from "./components/Chat";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
@@ -23,15 +21,12 @@ const App = () => {
 
   return (
     <div>
-<<<<<<< HEAD
       <Navigation />
       <Header data={landingPageData.Header} />
       <About data={landingPageData.About} />
       <Team data={landingPageData.Team} />
       <SignUp/>
-=======
       <Chat />
->>>>>>> efb17279cab27936901b109088a51f34b58beb4c
     </div>
   );
 };
